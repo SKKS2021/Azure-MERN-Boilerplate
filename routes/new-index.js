77f3e3648d75201ed16ed4ce8bd1287ae24e88fd
SKKS2021 <91369925+SKKS2021@users.noreply.github.com>
2021-09-25 21:24:53 -0700

    Update new-index.js
    added new strings

--- routes/new-index.js
+++ routes/new-index.js
@@ -3,13 +3,13 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
+var url = "mongodb://systemdev:8cwgKwygaKBVIQHjQwJppgAN6V5tlmgSPGYeTE7bbwkiTlcFBweidntfaqKuwg1dFMX2Oe3wj45kX64uCQJx4Q==@systemdev.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@systemdev@";
 
 
 router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("<database>");
+		var dbo = db.db("<systemdev>");
 		dbo.collection("collection1").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
